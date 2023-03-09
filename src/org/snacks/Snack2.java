23a4460af6598f3afb14bb3415a03cc191bb36ca
SaverioCascelli <saverio.cascelli@gmail.com>
2023-03-09 14:27:28 +0100

    [add] second Snack Complete

--- src/org/snacks/Snack2.java
+++ src/org/snacks/Snack2.java
@@ -1,4 +1,18 @@
 package org.snacks;
 
 public class Snack2 {
+
+//    Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+
+    public static void main(String[] args) {
+
+        String[] nameArr = {"Filippo","Piero","Andrea","Mario","Gianluca"};
+        String[] lastNameArr = {"Rossi","Bianchi","Stelluti","Mariani","Alberti",};
+
+        for (int i = 0; i < nameArr.length; i++) {
+            System.out.println(nameArr[i] + " " + lastNameArr[i]);
+        }
+    }
+
+
 }
