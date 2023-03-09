be922e7dd15e0fa921327bf69b4cc550e4b5cd88
SaverioCascelli <saverio.cascelli@gmail.com>
2023-03-09 14:18:40 +0100

    [add] first snack complete

--- src/org/snacks/Snack1.java
+++ src/org/snacks/Snack1.java
@@ -1,4 +1,18 @@
 package org.snacks;
 
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Snack1 {
+    public static void main(String[] args) {
+        //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero");
+        int number = parseInt(scan.nextLine());
+
+        System.out.println((number%2 == 0) ? number : number + 1);
+    }
 }
